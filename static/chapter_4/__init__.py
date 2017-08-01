485d145c12d6807828f087fd6ac152a14dd1de89
xugr <xugr>
2017-08-02 00:12:34 +0800

    Upload file about the python base's study.

--- static/chapter_4/__init__.py
+++ static/chapter_4/__init__.py
@@ -27,3 +27,62 @@ print('-----------------------------------------')
 M = [[1,2,3],[4,5,6],[7,8,9]]
 print(M)
 print(M[1][2])
+
+cols = [row[1] for row in M]
+print(cols)
+
+cols1 = [row[1] +1 for row in M]
+print(cols1)
+
+cols2 = [row[1] for row in M if row[1] % 2 != 0]
+print(cols2)
+
+diag = [M[i][i] for i in [0,1,2]]
+print(diag)
+
+doubles = [c * 2 for c in 'spam']
+print(doubles)
+
+G = (sum(row) for row in M)
+print(next(G))
+print(next(G))
+
+print("----------------------")
+print(list(map(sum,M)))
+
+print("create a set of row sums")
+# create a set of row sums
+tem = {sum(row) for row in M}
+print(tem)
+
+print("create key/value table of row sums")
+# create key/value table of row sums
+tem2 = {i : sum(M[i]) for i in range(3)}
+print(tem2)
+
+print("List of character ordinals")
+tem3 = [ord(x) for x in "spaam"]
+print(tem3)
+
+print("Set remove duplicates")
+tem4 = {ord(x) for x in "spaam"}
+print(tem4)
+
+print("Dictionary keys are unique")
+tem5 = {x : ord(x) for x in "spmmmd"}
+print(tem5)
+
+D = {'food':'Spam', 'quantity':4, 'color':'pink'}
+print('Fatch value of key \'food\'')
+print(D['food'])
+
+print('Add 1 to \'quantity\' value')
+D['quantity'] += 1
+print(D)
+
+F = {}
+print('create keys by assignment')
+F['name']='xugr'
+F['sex']='fale'
+print(F)
+
---
+++ static/chapter_4/_init_2.py
@@ -0,0 +1,28 @@
+D = {'a':1, 'b':2, 'c':3}
+Ks = list(D.keys())
+print('Unordered keys list')
+print(Ks)
+
+Ks.sort()
+print('keys sorted!')
+print(Ks)
+
+for key in Ks:
+    print(key, '=>', D[key])
+
+print(D)
+for key in sorted(D):
+    print(key, '=>', D[key])
+
+x = 4
+while x>0:
+    print('spam!' * x)
+    x -= 1
+
+squares = [x ** 2 for x in [1,2,3,4,5]]
+print(squares)
+print('it is equal as below! for loop')
+squares = []
+for x in [1,2,3,4,5]:
+    squares.append(x ** 2)
+print(squares)
